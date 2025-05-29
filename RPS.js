1d93b0940f78349e6b0f174888cd406b00ab2263
Chris D <mrcdean16@gmail.com>
2025-05-29 14:25:42 -0500

    Added function to prompt user for their choice

--- RPS.js
+++ RPS.js
@@ -1,12 +1,23 @@
 function getComputerChoice() {
-    testValue = Math.random() * 100;
-    if (testValue < 33) {
+    const randomNum = Math.random() * 100;
+    if (randomNum < 33) {
+        console.log(randomNum);
         return "rock";
     }
-    else if (testValue < 66) {
+    else if (randomNum < 66) {
+        console.log(randomNum);
         return "paper";
     }
+    console.log(randomNum);
     return "scissors";
 }
 
+function getHumanChoice() {
+    let playerChoice = prompt("Pick a value:");
+    return playerChoice;
+}
+
+
 console.log(getComputerChoice());
+console.log(getHumanChoice());
+
